a50fff915d5d190bc1841a57e349eab611ece909
carrot0822 <a384713827@qq.com>
2020-01-09 10:45:04 +0800

    更改逻辑

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -7,7 +7,7 @@ let axios = App.axios
 let Store = App.Store
 let Router = App.Router
 let isAuth = Store.getItem('isAuth')
-let colorArr = ['ff2424','ff6021','ff9b24','ffba13','fbc133']
+let colorArr = ['ff2424', 'ff6021', 'ff9b24', 'ffba13', 'fbc133']
 Page({
   data: {
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
@@ -23,13 +23,13 @@ Page({
     newBook: [],
     change: 0, // 左右判定
     swiperArr: [],
-    linkArr:[], // 广告
+    linkArr: [], // 广告
     place: "", // 位置 
-    
+
   },
   //API处理
-  imgError(e){
-    console.log('baocuo',e)
+  imgError(e) {
+    console.log('baocuo', e)
   },
   //事件处理函数
   change(e) {
@@ -39,20 +39,23 @@ Page({
     })
     console.log(value, '点击的值是什么')
   },
-  toLink(e){
-    let {linkType,link} = e.currentTarget.dataset.link
+  toLink(e) {
+    let {
+      linkType,
+      link
+    } = e.currentTarget.dataset.link
     let obj = {}
-    
-    if(linkType == '0'){
+
+    if (linkType == '0') {
       obj.id = link
       Router.push({
-        path:"article",
-        query:obj,
-        openType:'nav'
+        path: "article",
+        query: obj,
+        openType: 'nav'
       })
-    }else{
+    } else {
       wx.navigateTo({
-        url:`/pages/link/link?link=${link}`
+        url: `/pages/link/link?link=${link}`
       })
       /* obj.link = link
       Router.push({
@@ -61,7 +64,7 @@ Page({
         openType:'nav'
       }) */
     }
-    console.log('链接跳转',e)
+    console.log('链接跳转', e)
   },
   // 扩展四个小功能
   toBorrow() {
@@ -123,7 +126,7 @@ Page({
   tohotBro() {
     console.log('前往热门借阅')
   },
-  init(){
+  init() {
     this._banner()
     this._hot()
     this._recommend()
@@ -131,14 +134,14 @@ Page({
     this.initPlace()
   },
   // API
-  _banner(params = {}){
+  _banner(params = {}) {
     let url = Ip + Api.index.banner
     let data = params
     axios(url, data, 'GET').then((res) => {
       if (res.state) {
-        console.log(res.row.linkArr,'广告')
+        console.log(res.row.linkArr, '广告')
         this.setData({
-          linkArr:res.row.linkArr
+          linkArr: res.row.linkArr
         })
       }
     })
@@ -152,10 +155,10 @@ Page({
       if (res.state) {
 
         let arr = res.row.slice(0, 5)
-        for(let [index,item] of arr.entries()){
+        for (let [index, item] of arr.entries()) {
           item.color = colorArr[index]
         }
-        console.log(arr,'过滤后的热门')
+        console.log(arr, '过滤后的热门')
         this.setData({
           hotRead: arr
         })
@@ -221,7 +224,12 @@ Page({
       this.setData({
         place: value.name
       })
+      this._recommend()
+      this._newBook()
+      this._hot()
+      console.log(value, )
     })
+
   },
   _login(params = {}) {
     let url = Ip + Api.index.login
@@ -238,7 +246,7 @@ Page({
   // 过滤函数
   filterNull(obj) {
     for (let key in obj) {
-      if (obj[key]||key=="coverPhotoUrl") {
+      if (obj[key] || key == "coverPhotoUrl") {
 
       } else {
         obj[key] = '无数据'
@@ -255,24 +263,22 @@ Page({
   },
   // 钩子函数 有点意思 onshow和onload vue里或者浏览器端有这个吗
   onLoad: function () {
-  /*   if (isAuth) {
-      this._recommend()
-    } */
+    /*   if (isAuth) {
+        this._recommend()
+      } */
     this.initPlace()
     this._banner()
-    
-    this._recommend()
-    this._newBook()
-    this._hot()
+
+
     console.log(App)
     console.log('进入首页')
   },
   onShow() {
     // 判断
     let fromLib = wx.getStorageSync('fromLib')
-    if(fromLib){
+    if (fromLib) {
       this.init()
-      Store.setItem('fromLib',false)
+      Store.setItem('fromLib', false)
       console.log('从馆藏选择过来的')
     }
     let isAuth = wx.getStorageSync('isAuth');
@@ -280,14 +286,14 @@ Page({
       isAuth: isAuth
     })
   },
-  onReady(){
+  onReady() {
 
   },
   // 初始化参数
   initPlace() {
     let value = wx.getStorageSync('lib').name
-    let isAuth = wx.getStorageSync('isAuth')||0;
-    console.log(value,'nmd值应该拿到了啊')
+    let isAuth = wx.getStorageSync('isAuth') || 0;
+    console.log(value, 'nmd值应该拿到了啊')
     if (value) {
       wx.setNavigationBarTitle({
         title: value
@@ -295,6 +301,9 @@ Page({
       this.setData({
         place: value
       })
+      this._recommend()
+      this._newBook()
+      this._hot()
       console.log('值拿的到的吧', value)
     } else {
       this._libSelect()
